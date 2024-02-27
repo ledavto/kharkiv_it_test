3a4eadf320d02c3101ffe8bb6090568f2b589bce
ledavto <ser_ega@ukr.net>
2024-02-27 21:07:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ import mongoose from "mongoose";
 import { app } from "./app.js";
 import dotenv from "dotenv";
 
-dotenv.config();
-const { DB_HOST } = process.env; // Из файла .env
+// dotenv.config();
+// const { DB_HOST } = process.env; // Из файла .env
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(3000);
-    console.log("Server is running. Use our API on port: 3000");
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(3000);
+//     console.log("Server is running. Use our API on port: 3000");
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
 
 // Sergii     GeZ3SIUFrS09oG2P
 // npm install mongoose --save
